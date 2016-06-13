da91b03faea0ed6dac91446351371317d9b1b855
fernandoGuisso <fguisso@gmail.com>
2016-06-13 16:01:28 -0300

    add orderbook

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,52 @@
-var fb_data = {};
-function retriveJson (data) {
-	//console.log(data);
-	fb_data = data;
-}
-var js = document.createElement('SCRIPT');
-js.src = 'https://api.blinktrade.com/api/v1/BRL/ticker?callback=retriveJson';
-var head = document.getElementsByTagName('head')[0];
-head.insertBefore(js, head.childNodes[0]);
-window.onload = function (){
-	document.getElementById('low').innerHTML = fb_data.low;
-	document.getElementById('high').innerHTML = fb_data.high;
-	document.getElementById('last').innerHTML = fb_data.last;
-	document.getElementById('buy').innerHTML = fb_data.buy;
-	document.getElementById('sell').innerHTML = fb_data.sell;
-	document.getElementById('vol').innerHTML = fb_data.vol;
-};
+//without Angular
+
+// var ticker = {}, bids = {}, asks = {}, ob = {};
+// function retriveTicker (data) {
+// 	//console.log(data);
+// 	ticker = data;
+// };
+// function retriveBook(data){
+// 	bids = data.bids;
+// 	asks = data.asks;
+// 	console.log(bids);
+// 	//console.log(asks);
+// 	ob = data;
+// 	//console.log(ob.asks[0]);
+// };
+// var tickerJs = document.createElement('SCRIPT');
+// tickerJs.src = 'https://api.blinktrade.com/api/v1/BRL/ticker?callback=retriveTicker';
+// var bookJs = document.createElement('SCRIPT');
+// bookJs.src = 'https://api.blinktrade.com/api/v1/BRL/orderbook?callback=retriveBook';
+// var head = document.getElementsByTagName('head')[0];
+// head.insertBefore(tickerJs, head.childNodes[0]);
+// head.insertBefore(bookJs, head.childNodes[0]);
+// window.onload = function (){
+// 	document.getElementById('low').innerHTML = ticker.low;
+// 	document.getElementById('high').innerHTML = ticker.high;
+// 	document.getElementById('last').innerHTML = ticker.last;
+// 	document.getElementById('buy').innerHTML = ticker.buy;
+// 	document.getElementById('sell').innerHTML = ticker.sell;
+// 	document.getElementById('vol').innerHTML = ticker.vol;
+// };
+
+var app = angular.module("myApp", []);
+app.controller('jsonController', function($scope, $http){
+		var url = 'https://api.blinktrade.com/api/v1/BRL/orderbook?callback=JSON_CALLBACK';
+		$http.jsonp(url)
+			.success(function(data){
+				$scope.bids = data.bids;
+				$scope.asks = data.asks;
+			})
+			.error(function(err){
+				console.log("API orderbook: " + err);
+			});
+		var url = 'https://api.blinktrade.com/api/v1/BRL/ticker?callback=JSON_CALLBACK';
+		$http.jsonp(url)
+			.success(function(data){
+				$scope.ticker = data;
+			})
+			.error(function(err){
+				console.log("API ticker: " + err);
+			});
+
+});
